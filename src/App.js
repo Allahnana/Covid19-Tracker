37accf26edf411bc68610461c2a834e9bfee8fac
Allahnana <usmanallahnana@gmail.com>
2023-02-06 00:01:09 +0100

    I never get api oo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+
 
 // import Card from "./components/Cards/Cards";
 // import Chart from "./components/Chart/Chart";
@@ -25,6 +28,12 @@ import styles from "./App.module.css";
 // CLASS COMPONENT
 
 class App extends React.Component {
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
+
+    
     render() {
         return (
             <div className={styles.container}>
